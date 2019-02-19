43cba1b355c156de01912f84fe0d2921cc7a7033
Toxblh <toxblh@gmail.com>
2019-02-19 23:56:21 +0000

    init commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+
+# Configure Windows WSL for Frontend Development
+
+Location .zshrc in windows
+
+Win+R > `%USERPROFILE%\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs\home`
+
+next open `%UbuntuUser%\.zshrc`
+
+Change bash to zsh
+in bash `nano ~/.bashrc` or in the folder from the previous point
+and put to top of the file
+
+```shell
+# Launch Zsh
+if [ -t 1 ]; then
+exec zsh
+fi
+```
---
+++ install.sh
@@ -0,0 +1,31 @@
+# nodeJS
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - > /dev/null
+sudo apt-get install -y nodejs > /dev/null
+echo 'node version'
+node --version
+
+# yarn update
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - > /dev/null
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list > /dev/null
+sudo apt-get update > /dev/null
+sudo apt-get install -y yarn > /dev/null
+echo 'yarn version'
+yarn --version
+
+#zsh
+sudo apt-get install -y zsh
+
+# Oh-my-zsh:
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Change shell:
+chsh -s $(which zsh)
+
+# powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
