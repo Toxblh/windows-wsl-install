47b34ed8ecf326f7b9885e74231f80dea60836ff
Toxblh <toxblh@gmail.com>
2019-05-26 17:19:18 +0100

    upd install

--- install.sh
+++ install.sh
@@ -1,22 +1,49 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# Add PPA
+
+## Python
+add-apt-repository ppa:deadsnakes/ppa -y > /dev/null
+
+## NodeJS
+### curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - > /dev/null
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add - > /dev/null
+echo 'deb https://deb.nodesource.com/node_10.x xenial main' > /etc/apt/sources.list.d/nodesource.list
+echo 'deb-src https://deb.nodesource.com/node_10.x xenial main' >> /etc/apt/sources.list.d/nodesource.list
+
+## Yarn
+curl -s https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - > /dev/null
+echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list
+## ZSH
+curl -s https://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/xUbuntu_16.04/Release.key | apt-key add - > /dev/null
+echo "deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-completions/xUbuntu_16.04/ /" > /etc/apt/sources.list.d/shells:zsh-users:zsh-completions.list
+
+# Update
+sudo apt-get update > /dev/null
+
 # git
-sudo apt update && sudo apt install -y git
+sudo apt-get install -y git > /dev/null
+echo 'Git version'
+git --version
 
 # nodeJS
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - > /dev/null
 sudo apt-get install -y nodejs > /dev/null
-echo 'node version'
+echo 'Node.JS version'
 node --version
 
 # yarn update
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - > /dev/null
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list > /dev/null
-sudo apt-get update > /dev/null
 sudo apt-get install -y yarn > /dev/null
-echo 'yarn version'
+echo 'Yarn version'
 yarn --version
 
 # zsh
-sudo apt-get install -y zsh
+sudo apt-get install -y zsh > /dev/null
+echo 'ZSH version'
+zsh --version
 
 # Oh-my-zsh:
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -34,6 +61,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # zsh-completions
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-completions/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/shells:zsh-users:zsh-completions.list"
-sudo apt-get update
 sudo apt-get install zsh-completions
